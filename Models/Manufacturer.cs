cdb851ed8791b2ec40bd1b8d0715527d602c2827
Kavita <bhardwajkavita019@gmail.com>
2020-02-05 19:38:34 -0500

    Completed Lab4

---
+++ Models/Manufacturer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoneApplication.Models
+{
+    public class Manufacturer
+    {
+        public Manufacturer()
+        {
+            ManufacturerName = "";
+            URL = "";
+            DateReleased = new DateTime();
+        }
+
+        [Key]
+        public int ManufacturerID { get; set; }
+        public String ManufacturerName { get; set; }
+        public String URL { get; set; }
+        public DateTime DateReleased { get; set; }
+        public virtual ICollection<Phone> PhoneList { get; set; }
+        
+        
+
+    }
+}
